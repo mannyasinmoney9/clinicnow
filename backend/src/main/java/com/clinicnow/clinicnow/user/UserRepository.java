2c0eedd35b2ec91648d959026a676a418f16ff21
mannyasinmoney9 <manniboh@gmail.com>
2026-07-03 20:44:51 +0000

    Jul 3, 2026, 9:44 PM

--- backend/src/main/java/com/clinicnow/clinicnow/user/UserRepository.java
+++ backend/src/main/java/com/clinicnow/clinicnow/user/UserRepository.java
@@ -1,11 +1,9 @@
 package com.clinicnow.clinicnow.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
