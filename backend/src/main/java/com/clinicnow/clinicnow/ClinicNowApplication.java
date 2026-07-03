cb9fefb78767a5700a0d89328ff4177e82816f87
mannyasinmoney9 <manniboh@gmail.com>
2026-07-03 10:38:23 +0000

    Jul 3, 2026, 11:38 AM

---
+++ backend/src/main/java/com/clinicnow/clinicnow/ClinicNowApplication.java
@@ -0,0 +1,13 @@
+package com.clinicnow.clinicnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicNowApplication.class, args);
+	}
+
+}
