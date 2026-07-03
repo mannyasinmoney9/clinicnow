e9d504cf0991710f6d434ac1a5ae26a415f2a04a
mannyasinmoney9 <manniboh@gmail.com>
2026-07-03 10:55:19 +0000

    Jul 3, 2026, 11:55 AM

--- backend-temp/src/main/java/com/example/demo/DemoApplication.java
+++ backend-temp/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.clinicnow.clinicnow;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ClinicNowApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ClinicNowApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
